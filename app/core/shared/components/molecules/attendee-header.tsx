c7b2fe1c91a5d20c809611a29225bbbee6620418
orsdev <oluwadareysamuel@gmail.com>
2025-10-14 21:25:51 +0100

    refactor: move sidebar toggle button from exhibitor to attendee header

--- app/core/shared/components/molecules/attendee-header.tsx
+++ app/core/shared/components/molecules/attendee-header.tsx
@@ -5,6 +5,8 @@ import { usePathname } from 'next/navigation';
 import { ATTENDEE_APP_ROUTES } from '../../constants';
 import { useAttendeeUser } from '../../hooks/api';
 import { AvatarMenu } from './avatar-menu';
+import { useSidebar } from '../atoms';
+import { AlignLeft } from 'lucide-react';
 
 const DASHBOARD_TITLES = {
   '/catalogues': 'Catalogues',
@@ -34,6 +36,7 @@ const getTitle = (pathname: string) => {
 };
 
 export const AttendeeHeader = () => {
+  const { toggleSidebar } = useSidebar();
   const { handleLogOut } = useAttendeeAuthStore();
   const { user } = useAttendeeUser();
   const pathname = usePathname();
@@ -41,7 +44,15 @@ export const AttendeeHeader = () => {
 
   return (
     <header className="flex justify-between items-center py-[1rem] gap-x-2 w-full relative border-b px-6">
-      <p className="sm:text-lg font-semibold text-foreground">{title}</p>
+      <div className="flex items-center gap-x-2">
+        <button
+          onClick={toggleSidebar}
+          className="text-foreground cursor-pointer"
+        >
+          <AlignLeft size={24} />
+        </button>
+        <p className="sm:text-lg font-semibold text-foreground ml-1">{title}</p>
+      </div>
       <div className="flex items-center gap-x-1">
         <AvatarMenu
           className="border-none"
